4989db5a81ed451f727a899517a22cbb4b69382d
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2023-02-22 09:12:05 +0700

    advance unit test using loadFixture

--- test/FloppyToken.spec.ts
+++ test/FloppyToken.spec.ts
@@ -1,23 +1,30 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-console.log("FloppyToken.spec.ts: 1")
+import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 
 describe("FloppyToken", async function () {
+	async function deployTokenFixture(){
+		const Token = await ethers.getContractFactory('Floppy');
+		const [owner, addr1, addr2] = await ethers.getSigners();
+		const hardhatToken = await Token.deploy();
+		await hardhatToken.deployed();
+		return {
+			Token, owner, addr1, addr2, hardhatToken
+		};
+	}
 	it("Deployment should assign the total supply of tokens to the owner", async function () {
-		const [owner] = await ethers.getSigners();
-		const FloppyToken = await ethers.getContractFactory("Floppy");
-		const hardhatToken = await FloppyToken.deploy();
+		const {hardhatToken, owner} = await loadFixture(deployTokenFixture);
 		const ownerBalance = await hardhatToken.balanceOf(owner.address);
 		expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
 	})
 	it("Should transfer tokens between accounts", async function () {
-		const [owner, addr1, addr2] = await ethers.getSigners();
-		const FloppyToken = await ethers.getContractFactory("Floppy");
-		const hardhatToken = await FloppyToken.deploy();
-		await hardhatToken.transfer(addr1.address, 50);
-		expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
-		await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-		expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+		const {hardhatToken, owner, addr1, addr2} = await loadFixture(deployTokenFixture);
+		await expect(
+			hardhatToken.transfer(addr1.address, 50)
+		).to.changeTokenBalances(hardhatToken, [owner, addr1], [-50,50]);
+		await expect(
+			hardhatToken.connect(addr1).transfer(addr2.address,50)
+		).to.changeTokenBalances(hardhatToken, [addr1, addr2], [-50,50]);
 	})
 }
 )
