e8089fef209aaa2c0a2eb66601ae38b031f88d5c
Huynh Ba Huy <19127420@student.hcmus.edu.vn>
2023-02-22 09:01:58 +0700

    basic test case

--- test/FloppyToken.spec.ts
+++ test/FloppyToken.spec.ts
@@ -10,4 +10,14 @@ describe("FloppyToken", async function () {
 		const ownerBalance = await hardhatToken.balanceOf(owner.address);
 		expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
 	})
-})
+	it("Should transfer tokens between accounts", async function () {
+		const [owner, addr1, addr2] = await ethers.getSigners();
+		const FloppyToken = await ethers.getContractFactory("Floppy");
+		const hardhatToken = await FloppyToken.deploy();
+		await hardhatToken.transfer(addr1.address, 50);
+		expect(await hardhatToken.balanceOf(addr1.address)).to.equal(50);
+		await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+		expect(await hardhatToken.balanceOf(addr2.address)).to.equal(50);
+	})
+}
+)
